23e2c799d26b6ede9d007c5fc6494c066ae35b23
William Sullivan <sullivanwp95@gmail.com>
2023-03-24 15:53:15 -0400

    Refactor function to fill canvas with a specific number squares

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-// const canvas = document.querySelector('#canvas')
+const canvas = document.querySelector('#canvas')
 
-// function fillCanvas(num) {
-//     for (let i = 0; i < num * num; i++) {
-//         let div = document.createElement('div')
-//         canvas.appendChild(div)        
-//     }
-// }
+function fillCanvas(num) {
 
-// fillCanvas(16)
+    // adds (num) divs with class of 'container' to the canvas div to act as columns in our grid
+    for (let i = 0; i < num; i++) {
+        let div = document.createElement('div')
+        div.classList.add('container')
+        canvas.appendChild(div)
+
+        // adds (num) divs with class of 'square' to each 'container' div in order to fill each row with a square
+        for (let j = 0; j < num; j++) {
+            let container = document.querySelectorAll('.container')
+            let square = document.createElement('div')
+            square.classList.add('square')
+            container[i].appendChild(square)
+        }
+    }  
+}
+
+fillCanvas(16)
